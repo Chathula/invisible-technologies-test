c9b2b533d0efc81c7d8efd8c3558bd696a000fb0
chathula <schathula@gmail.com>
2019-02-16 15:56:23 +0530

    make application with sample dataset

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import config from './config';
+import axios from 'axios';
+
+interface location {
+  location_name: string,
+  postal_code: string | number,
+  weather?: string,
+  current_time?: string
+}
+
+// sample dataset
+let inputData: Array<location> = [
+  { location_name: 'Los Angeles', postal_code: '90001' },
+  { location_name: 'Washington', postal_code: '20001' },
+  { location_name: 'Atlanta', postal_code: '30301' },
+  { location_name: 'New York', postal_code: '10001' },
+  { location_name: 'Oxford', postal_code: '27565' },
+  { location_name: 'Newport', postal_code: '02840' },
+  { location_name: 'Camden', postal_code: '29020' },
+  { location_name: 'Killington', postal_code: '05751' },
+  { location_name: 'Altavista', postal_code: '24517' },
+  { location_name: 'Beaver', postal_code: '25813' },
+];
+
+for (const input of inputData) {
+  (async () => {
+    const response: location | null = await fetchWeatherData(input);
+    console.log(response);
+  })();
+}
+
+
+async function fetchWeatherData(locationInput: location): Promise<location|null> {
+  try {
+    const url: string = `${config.API_ENDPOINT}?key=${config.API_KEY}&format=json&num_of_days=0&q=${locationInput.postal_code}&extra=localObsTime`;
+    const { data } = await axios.get(url);
+    return { ...locationInput, weather: data.data.current_condition[0].weatherDesc[0].value, current_time: data.data.current_condition[0].localObsDateTime };
+  } catch (e) {
+    return null;
+  }
+}
